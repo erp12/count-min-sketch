a392e014c3aa5ed84f0a69b9d683e80ad839e9d7
erp12 <EPantridge@MassMutual.com>
2016-11-09 14:45:53 -0500

    Upgraded ds-util version

--- project.clj
+++ project.clj
@@ -1,12 +1,11 @@
-
-(defproject count-min-sketch "0.1.1"
+(defproject count-min-sketch "0.1.2"
   :description "A simple Count-Min Sketch implementation in Clojure."
   :url "https://github.com/erp12/count-min-sketch"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/math.numeric-tower "0.0.4"]
-                 [ds-utils "0.1.0"]]
+                 [ds-utils "0.1.1"]]
   :plugins [[org.clojars.benfb/lein-gorilla "0.4.0"]]
   :main ^:skip-aot count-min-sketch.core
   :target-path "target/%s"
