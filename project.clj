12c3d5c12080d2778470226699c101cd186dc41d
erp12 <EPantridge@MassMutual.com>
2016-11-09 10:50:45 -0500

    Update project url and description

--- project.clj
+++ project.clj
@@ -1,7 +1,8 @@
 
+
 (defproject count-min-sketch "0.1.0"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "A simple Count-Min Sketch implementation in Clojure."
+  :url "https://github.com/erp12/count-min-sketch"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
@@ -11,3 +12,4 @@
   :main ^:skip-aot count-min-sketch.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
+
